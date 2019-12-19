348b0182a58250c1f80deb20e415f7fa16efb1b2
Ninte <ninte.dev@gmail.com>
2019-12-19 23:05:12 +0000

    added query

---
+++ 020.sql
@@ -0,0 +1,4 @@
+-- all the orders where the standard_qty is over 1000, the poster_qty is 0, and the gloss_qty is 0
+SELECT *
+FROM orders
+WHERE standard_qty > 1000 AND poster_qty = 0 AND gloss_qty = 0;

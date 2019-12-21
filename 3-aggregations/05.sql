7efa6643948de61d7d2bded1f2de0a0928bd9f69
Ninte <ninte.dev@gmail.com>
2019-12-21 17:18:24 +0000

    edited query

--- 3-aggregations/05.sql
+++ 3-aggregations/05.sql
@@ -1,2 +1,7 @@
+/*
 SELECT SUM(standard_amt_usd/(standard_qty + 0.01)) AS total_unit_price
+FROM orders;
+*/
+
+SELECT SUM(standard_amt_usd)/SUM(standard_qty) AS standard_price_per_unit
 FROM orders;

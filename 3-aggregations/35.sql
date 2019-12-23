2853d2d4967c43a44444b3003e92a73dc592771e
Ninte <ninte.dev@gmail.com>
2019-12-23 09:49:27 +0000

    added queries

---
+++ 3-aggregations/35.sql
@@ -0,0 +1,7 @@
+SELECT account_id,
+	total,
+    CASE WHEN total >= 2000 THEN 'At Least 2000'
+    	WHEN total > 1000 AND total < 2000 THEN 'Between 1000 and 2000'
+        WHEN total <= 1000 THEN 'Less than 1000'
+        END AS order_quantity
+FROM orders;
